318db56bad637bb3e3c4b1264cb96498da341476
Nicholas Shephard <shephardn@gmail.com>
2021-12-16 09:55:03 -0600

    commit tree fde55857f2e6d84e6232e1d21189e7d09d6c9f0e

--- lib/person.rb
+++ lib/person.rb
@@ -13,14 +13,73 @@ class Person
     end
 
     def hygiene=(value)
-        value > 10 ? (puts "Nobody's THAT clean") : @hygiene=value
-        binding.pry
+        if value > 10
+            puts "Nobody's THAT clean"
+            @hygiene=10
+        elsif value < 0
+            puts "Nobody's THAT dirty"
+            @hygiene=0
+        else
+            @hygiene=value
+        end
     end
 
     def happiness=(value)
-        value > 10 ? (puts "You can only be SO happy") : @happiness=value
+        if value > 10
+            puts "Nobody's THAT happy"
+            @happiness=10
+        elsif value < 0
+            puts "Nobody's THAT sad"
+            @happiness=0
+        else
+            @happiness=value
+        end
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account = self.bank_account + amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = self.hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness = self.happiness + 2
+        self.hygiene = self.hygiene - 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness = self.happiness + 3
+        friend.happiness = friend.happiness + 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness = self.happiness - 2
+            friend.happiness = friend.happiness - 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness = self.happiness + 1
+            friend.happiness = friend.happiness + 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
     end
 
 end
 
-binding.pry
+# binding.pry
